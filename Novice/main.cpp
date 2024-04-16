560bdb7fec99bd712938fdeb352724a07b50d310
k023g1018-hidemasa <147122260+k023g1018-hidemasa@users.noreply.github.com>
2024-04-16 13:06:58 +0900

    00-01を作成

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -1,6 +1,70 @@
 #include <Novice.h>
+#include <math.h>
 
 const char kWindowTitle[] = "GC2B_15_ヒラジマ_ヒデマサ＿MT3";
+
+static const int kColumnWidth = 60;
+static const int kRowHight = 20;
+
+struct Vector3 { // ヴェクター3
+	float x, y, z;
+};
+
+Vector3 Add(const Vector3& v1, const Vector3& v2) {
+	Vector3 result{};
+	result.x = v1.x + v2.x;
+	result.y = v1.y + v2.y;
+	result.z = v1.z + v2.z;
+	return result;
+};
+
+Vector3 Subtract(const Vector3& v1, const Vector3& v2) {
+	Vector3 result{};
+	result.x = v1.x - v2.x;
+	result.y = v1.y - v2.y;
+	result.z = v1.z - v2.z;
+	return result;
+};
+
+Vector3 Multiply(float scalar, const Vector3& v) {
+	Vector3 result{};
+	result.x = scalar * v.x;
+	result.y = scalar * v.y;
+	result.z = scalar * v.z;
+	return result;
+};
+
+float Dot(const Vector3& v1, const Vector3& v2) {
+	float result;
+	result = (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
+	return result;
+	// あってんのか知らん//なんかでぇへん
+}
+
+float Length(const Vector3& v) {
+	float result;
+	result = sqrtf((v.x * v.x) + (v.y * v.y) + (v.z * v.z));
+	return result;
+	// 足すのかかけるのかは知らん
+}
+
+Vector3 Normalize(const Vector3& v) {
+	Vector3 result{};
+	float mag = 1 / sqrtf((v.x * v.x) + (v.y * v.y) + (v.z * v.z));///長さわる１してそれかける元の数
+	result.x = mag * v.x;
+	result.y = mag * v.y;
+	result.z = mag * v.z;
+	return result;
+	///??????
+};
+
+/////////表示の関数
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+};
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -11,6 +75,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	Vector3 v1{1.0f, 3.0f, -5.0f};
+	Vector3 v2{4.0f, -1.0f, 2.0f};
+	float k = {4.0f};
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -25,6 +93,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Vector3 resultAdd = Add(v1, v2);
+		Vector3 resultSubtract = Subtract(v1, v2);
+		Vector3 resultMultiply = Multiply(k, v1);
+		float resultDot = Dot(v1, v2);
+		float resultLength = Length(v1);
+		Vector3 resultNormalize = Normalize(v2);
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +107,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		VectorScreenPrintf(0, 0, resultAdd, ":Add");
+		VectorScreenPrintf(0, kRowHight, resultSubtract, ":subtract");
+		VectorScreenPrintf(0, kRowHight * 2, resultMultiply, ":Multiply");
+		Novice::ScreenPrintf(0, kRowHight * 3, "%.02f  :dot", resultDot);
+		Novice::ScreenPrintf(0, kRowHight * 4, "%.02f  :Length", resultLength);
+		VectorScreenPrintf(0, kRowHight * 5, resultNormalize, ":Normalize");
 
 		///
 		/// ↑描画処理ここまで
