e54db0f131b7a649c13db3cdcbb97e6b6d9c7111
k023g1018-hidemasa <147122260+k023g1018-hidemasa@users.noreply.github.com>
2024-06-11 11:37:02 +0900

    分からんとこは書いた後は頼んだ

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -18,16 +18,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Vector3 v2{2.8f, 0.4f, -1.3f};
 	Vector3 cross = Cross(v1, v2);
 
-	Vector3 rotate{};//ゼロで初期化
-	Vector3 translate{1.0f, 1.0f, 1.0f};
-	Vector3 cameraPosition{100.0f,100.0f,100.0f};//多分ALとのこと,なかった//ここは多分前後に動いたときの変数
+	Vector3 rotate{};//ゼロで初期化//ここで回る
+	Vector3 translate{1.0f, 1.0f, 1.0f};//コレででかくなる
+	Vector3 cameraPosition{100.0f,100.0f,100.0f};//多分ALとのこと,なかった//ここは多分前後に動いたときの変数？？？？？
+	//↑じゃこれ何？ここの数字いじったら座標おかしくなった
 	Vector3 kLocalVertices[3]{3.0f, 3.0f, 1.0f,//ここを回転させるときに動く多分最初は定数でヨシ
 		1.0f, 6.0f, 1.0f, 
-		6.0f, 6.0f, 1.0f};
+		6.0f, 6.0f, 1.0f};//多分ここで三角形の大まかなでかさ決めてると思われ
 	
 
 
+	//今これどうゆう状況？なんで設定した値より数倍でかいしどっか行ってんの？カメラポジションて何？
+	//回転する場所のいじるいじらないがある？、
 
+	
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -63,11 +67,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	Matrix4x4 worldMatrix = MakeAffineMatrix({1.0f, 1.0f, 1.0f}, rotate, translate);
 	Matrix4x4 cameraMatrix = MakeAffineMatrix({1.0f, 1.0f, 1.0f}, {0.0f, 0.0f, 0.0f}, cameraPosition);
+	//↑こいつが悪い気がするこれマジで意味わからん
 	Matrix4x4 viewMatrix = Inverse(cameraMatrix);
 	Matrix4x4 projectionMatrix = MakePerspectiveMatrix(0.45f, float(kWindowWidth) / float(kWindoweHeight), 0.1f, 100.0f);
 	Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
 	Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindoweHeight), 0.0f, 1.0f);
 	Vector3 screenVertices[3];
+	//移すは移した
 	for (uint32_t i = 0; i < 3; ++i) {
 		Vector3 ndcVertex = Transform(kLocalVertices[i], worldViewProjectionMatrix);
 		screenVertices[i] = Transform(ndcVertex, viewportMatrix);
@@ -86,7 +92,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Novice::DrawTriangle(
 		     int(screenVertices[0].x), int(screenVertices[0].y)
 			,int(screenVertices[1].x), int(screenVertices[1].y)
-			,int(screenVertices[2].x),int(screenVertices[2].y),RED,kFillModeSolid
+			,int(screenVertices[2].x), int(screenVertices[2].y)
+			,RED,kFillModeSolid
 		);
 		/*Novice::DrawTriangle(
 		    300, 300,
