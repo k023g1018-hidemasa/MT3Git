fb37e3f00a2a5645a6ffebcb37e7e85f47a399b0
k023g1018-hidemasa <147122260+k023g1018-hidemasa@users.noreply.github.com>
2024-06-24 17:46:33 +0900

    エラー吐てるよくわからん

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -49,14 +49,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
+	Vector3 rotate{};
+	Vector3 translate{};
+	Vector3 cameraRotate = {0.26f};
+	Vector3 cameraPosition{0.0f, 1.9f, -6.49f}; 
 	Segment segment{
 	    {-2.0f, -1.0f, 0.0f},
         {3.0f,  2.0f,  2.0f}
     };
 	Vector3 point{-1.5f, 0.0f, 0.0f};
-
+	Sphere sphere = {1.0f, 1.0f, 1.0f};
+	1.0f;
 	Vector3 project = Project(Subtract(point, segment.origin), segment.diff);
 	Vector3 closetPoint = ClosestPoint(point, segment);
+
+	Sphere pointSphere{point, 0.01f};
+	Sphere closestPointSphere{closetPoint, 0.01f};
+	uint32_t color = 0xafaaaaff;
 
 
 	// ウィンドウの×ボタンが押されるまでループ
@@ -73,6 +82,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 
+		Matrix4x4 worldMatrix = MakeAffineMatrix({1.0f, 1.0f, 1.0f}, rotate, translate);
+		Matrix4x4 cameraMatrix = MakeAffineMatrix({1.0f, 1.0f, 1.0f}, cameraRotate, cameraPosition);
+		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+		Matrix4x4 projectionMatrix = MakePerspectiveMatrix(0.45f, float(kWindowWidth) / float(kWindoweHeight), 0.1f, 100.0f);
+		Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
+		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindoweHeight), 0.0f, 1.0f);
+
+	Vector3 start = Transform(Transform(segment.origin, viewProjectionMatrix), viewportMatrix);
+		Vector3 end = Transform(Transform(Add(segment.origin, segment.diff), viewProjectionMatrix), viewportMatrix);
+
+
+
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -80,6 +102,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+
+		Novice::DrawLine(int(start.x), int(start.y), int(end.x), int(end.y), WHITE);
+
+
+
+
+		DrawSphere(pointSphere,viewportMatrix,viewportMatrix,RED);
+		DrawSphere(closestPointSphere, viewProjectionMatrix, viewportMatrix, BLACK);
 
 		
         ///	
