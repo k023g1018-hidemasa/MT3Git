9db0e578a1006b5ea24ba46f19f904dc7faca252
k023g1018-hidemasa <147122260+k023g1018-hidemasa@users.noreply.github.com>
2024-07-18 18:48:23 +0900

    うｐ

--- Novice/MTFunction.h
+++ Novice/MTFunction.h
@@ -41,6 +41,21 @@ struct Plane {
 	Vector3 normal;
 	float distance;
 };
+bool PlaneIsCollision(const Sphere& sphere, const Plane& plane) {
+	float d = plane.distance;
+	// nが法線の向き？ならｄはｎじゃないの？
+	Vector3 n = plane.normal;
+
+	Vector3 c = sphere.center;
+	float k; // 平面と中心点の距離らしい
+	k = Dot(n, c) - d;
+	// ここの変換てどうすんの｜中身↑｜らしいから多分返還しろってこと
+	// 関数の中身知るかヴォケお前らみたいに脳みそ詰まってないんじゃ人のことも考えろや自己中どもが
+
+	return abs(k) <= sphere.radius;
+	// 焼身
+}
+
 
 
 Vector3 Cross(const Vector3& v1, const Vector3& v2) {
@@ -413,7 +428,26 @@ void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, con
 	}
 	
 }
+void DrawPlane(const Plane& plane, const Matrix4x4& viewProjectionMatrix, const Matrix4x4 viewportMatrix, uint32_t color) {
+	Vector3 center = Scale(plane.distance, plane.normal); // 変換がない何処にかいてんねん
+	Vector3 perpendicular[4];
+	perpendicular[0] = Normalize(Perpendicular(plane.normal));
+	perpendicular[1] = {-perpendicular[0].x, -perpendicular[0].y, -perpendicular[0].z};
+	perpendicular[2] = Cross(plane.normal, perpendicular[0]);
+	perpendicular[3] = {-perpendicular[2].x, -perpendicular[2].y, -perpendicular[2].z};
 
+	Vector3 points[4];
+	for (int32_t i = 0; i < 4; ++i) {
+		Vector3 extend = Scale(2.0f, perpendicular[i]);
+		Vector3 point = Add(center, extend);
+		points[i] = Transform(Transform(point, viewProjectionMatrix), viewportMatrix);
+	}
+	Novice::DrawLine(int(points[3].x), int(points[3].y), int(points[1].x), int(points[1].y), color);
+	Novice::DrawLine(int(points[0].x), int(points[0].y), int(points[2].x), int(points[2].y), color);
+	Novice::DrawLine(int(points[0].x), int(points[0].y), int(points[3].x), int(points[3].y), color);
+	Novice::DrawLine(int(points[2].x), int(points[2].y), int(points[1].x), int(points[1].y), color);
+	// mazuuturan
+}
 
 
 
